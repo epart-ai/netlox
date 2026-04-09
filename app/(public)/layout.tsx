bc85b7867b36daf6e7050f8fe15b604c047b44a9
meehyunkim <139302009+meehyunkim@users.noreply.github.com>
2026-04-09 09:47:27 +0900

    design: header 구글태그 삽입

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import { Suspense } from "react";
 
 import "@fontsource-variable/outfit";
@@ -33,10 +34,33 @@ export default async function RootLayout({
 }>) {
 	return (
 		<html lang="ko" suppressHydrationWarning>
+			<head>
+				<Script
+					id="gtm-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WTMCMRT7');
+            `,
+					}}
+				/>
+			</head>
 			<body
 				suppressHydrationWarning
 				className={`${pretendardBold.variable} bg-blue-100 leading-[normal] text-white antialiased`}
 			>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-WTMCMRT7"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
 				<AppProviders>
 					<Header />
 					<main className="min-h-screen">{children}</main>
